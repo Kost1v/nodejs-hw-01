e767be69890c7c0b98b896e6381786f0bfd7bed2
Ostap <ostapcostiv@gmail.com>
2024-12-07 14:24:10 +0200

    node.task

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contactList = await readContacts();
+  const newContact = Array(number).fill(0).map(createFakeContact);
+  const newContactList = [...contactList, ...newContact];
+  await writeContacts(newContactList);
+};
 
 generateContacts(5);
